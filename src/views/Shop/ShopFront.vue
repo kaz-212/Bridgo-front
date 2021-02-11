912a6c39067999cc36ab56b391cd94456aeb1d19
Karan Basu <karan.basu16@gmail.com>
2021-02-11 17:20:48 +0000

    changed banner to take full width on scroll

--- src/views/Shop/ShopFront.vue
+++ src/views/Shop/ShopFront.vue
@@ -1,16 +1,18 @@
 <template>
   <div v-if="prints">
-    <div class="shop-banner">
-      <h1>SHOP</h1>
-      <div class="tabs">
-        <span
-          v-for="tab in tabs"
-          :key="tab.name"
-          class="tab-name"
-          @click="setTab(tab)"
-          :class="{ selected: selectedTab === tab.name }"
-          >{{ tab.name }}</span
-        >
+    <div class="fixed" :class="{ scrolling: scrolling }">
+      <div class="shop-banner">
+        <h1>SHOP</h1>
+        <div class="tabs">
+          <span
+            v-for="tab in tabs"
+            :key="tab.name"
+            class="tab-name"
+            @click="setTab(tab)"
+            :class="{ selected: selectedTab === tab.name }"
+            >{{ tab.name }}</span
+          >
+        </div>
       </div>
     </div>
     <section class="tab-body">
@@ -47,7 +49,8 @@ export default {
   data() {
     return {
       tabs: [],
-      selectedTab: 'Prints'
+      selectedTab: 'Prints',
+      scrolling: false
     }
   },
   computed: {
@@ -65,60 +68,92 @@ export default {
   },
   mounted() {
     this.tabs = [this.$refs.tab1, this.$refs.tab2, this.$refs.tab3]
+  },
+  created() {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 1) {
+        this.scrolling = true
+      } else {
+        this.scrolling = false
+      }
+    })
+  },
+  destroyed() {
+    window.removeEventListener('scroll', () => {
+      this.scrolling = false
+    })
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.shop-banner {
-  width: 100%;
+.fixed {
+  width: 84%;
+  height: 28vh;
   margin: 0 auto;
-  // margin-top: 7vh;
-  // border: 2px solid black;
-  // height: 25vh;
-  height: 33vh;
-  background-image: url('https://res.cloudinary.com/dqyymjqpg/image/upload/v1612955104/Bridgo/statics/6A91EEF0-A9E9-4198-9601-63D8350F2886_wefe5t.jpg');
-  // background-image: url('https://res.cloudinary.com/dqyymjqpg/image/upload/v1612954940/Bridgo/statics/FDD1FA05-A350-4B8B-9483-60A282CF2E05_oh629l.jpg');
-  background-size: cover;
-  background-position: center;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  position: relative;
-  h1 {
-    margin: 0;
-    color: rgba(255, 255, 255, 0);
-    font-size: 60px;
-    text-shadow: 0 0 2px #c3bf43;
-    font-weight: 300;
-  }
-  .tabs {
-    position: absolute;
-    bottom: 10px;
+  margin-top: 7vh;
+  position: fixed;
+  top: 0;
+  left: 8%;
+  transition: all 0.5s ease-out;
 
-    .tab-name {
-      margin: 0 30px;
-      font-size: 17px;
+  &.scrolling {
+    width: 100%;
+    height: 23vh;
+    left: 0;
+    top: 5vh;
+    margin: 0;
+  }
+
+  .shop-banner {
+    width: 100%;
+    height: 100%;
+    background-image: url('https://res.cloudinary.com/dqyymjqpg/image/upload/v1612955104/Bridgo/statics/6A91EEF0-A9E9-4198-9601-63D8350F2886_wefe5t.jpg');
+    // background-image: url('https://res.cloudinary.com/dqyymjqpg/image/upload/v1612954940/Bridgo/statics/FDD1FA05-A350-4B8B-9483-60A282CF2E05_oh629l.jpg');
+    background-size: cover;
+    background-position: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    transition: width 0.5s ease-out;
+
+    h1 {
+      margin: 0;
+      color: rgba(255, 255, 255, 0);
+      font-size: 60px;
+      text-shadow: 0 0 2px #c3bf43;
       font-weight: 300;
-      cursor: pointer;
-      color: black;
     }
-    .selected {
-      color: #ffff14d6;
-      // text-decoration: underline;
+    .tabs {
+      position: absolute;
+      bottom: 20px;
+
+      .tab-name {
+        margin: 0 30px;
+        font-size: 17px;
+        font-weight: 300;
+        cursor: pointer;
+        color: #ffff14d6;
+        letter-spacing: 2px;
+      }
+      .selected {
+        border-bottom: 1px solid #ffff14d6;
+      }
     }
   }
 }
 
 .shop-container {
-  padding-top: 4vh;
+  padding-top: 8vh;
   margin: 0 auto;
-  width: 60vw;
-  // background: black;
-  // background: linear-gradient(#9198e5, #e66465);
+  margin-top: 32vh;
+  width: 63vw;
   display: grid;
+  grid-gap: 25px;
   grid-template-columns: repeat(3, auto);
+
   min-height: 75vh;
 }
 </style>
