d23bf0f21b8e95e70836025b12f110807ebc4481
Karan Basu <karan.basu16@gmail.com>
2021-02-16 20:54:08 +0000

    created a standard button

---
+++ src/components/Buttons/StandardButton.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h4 class="button">{{ text }}</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'StandardButton',
+  props: { text: String }
+}
+</script>
+
+<style lang="scss" scoped>
+.button {
+  position: absolute;
+  bottom: 10vh;
+  right: 6vw;
+  border-bottom: 1px solid black;
+  border-left: 1px solid black;
+  padding: 5px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  transition: all 0.2s;
+
+  &:hover {
+    // border-bottom: rgb(197, 197, 197);
+    // border-left: rgb(197, 197, 197);
+    // border-top: 1px solid black;
+    // border-right: 1px solid black;
+
+    box-shadow: 0 0.5em 0.5em -0.4em #2f302c;
+    transform: translateY(-0.15em);
+    cursor: pointer;
+  }
+}
+</style>
--- src/components/Portfolio/PortfolioTab.vue
+++ src/components/Portfolio/PortfolioTab.vue
@@ -36,18 +36,20 @@
           :project="project"
         />
       </div>
-      <h4 class="toggle-view" @click="toggleScrollView">View All</h4>
+      <StandardButton @click="toggleScrollView" text="View All" />
     </section>
   </div>
 </template>
 
 <script>
 import PortfolioWindow from '@/components/Portfolio/PortfolioWindow.vue'
+import StandardButton from '@/components/Buttons/StandardButton.vue'
 
 export default {
   name: 'Portfolio',
   components: {
-    PortfolioWindow
+    PortfolioWindow,
+    StandardButton
   },
   props: { projects: Array },
   emits: ['toggle-scroll-view'],
@@ -161,29 +163,6 @@ export default {
 
       .destroy {
         display: none;
-      }
-    }
-
-    .toggle-view {
-      position: absolute;
-      bottom: 10vh;
-      right: 6vw;
-      border-bottom: 1px solid black;
-      border-left: 1px solid black;
-      padding: 5px;
-      letter-spacing: 1px;
-      cursor: pointer;
-      transition: all 0.2s;
-
-      &:hover {
-        // border-bottom: rgb(197, 197, 197);
-        // border-left: rgb(197, 197, 197);
-        // border-top: 1px solid black;
-        // border-right: 1px solid black;
-
-        box-shadow: 0 0.5em 0.5em -0.4em #2f302c;
-        transform: translateY(-0.15em);
-        cursor: pointer;
       }
     }
   }
