2b13969f6c429a7e072ab5180b1e43f5b8090b59
Karan Basu <karan.basu16@gmail.com>
2021-02-16 20:40:48 +0000

    basic basket page stylings

--- src/views/Shop/Basket.vue
+++ src/views/Shop/Basket.vue
@@ -1,47 +1,66 @@
 <template>
   <div v-if="basketItems">
-    <h1>Basket</h1>
-    <section>
-      <table>
-        <thead>
-          <tr>
-            <th>Item</th>
-            <th>Quantity</th>
-            <th>Price</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="(item, index) in basketItems" :key="item.particular._id">
-            <td>
-              {{ item.product.name }}
-              {{ item.particular.size != 'N/A' ? item.particular.size : '' }}
-            </td>
-            <td>
-              <input
-                @change="changeQuantity(item, $event)"
-                type="number"
-                :name="`${index}-qty`"
-                :id="`${index}-qty`"
-                :value="item.qty"
-                min="1"
-                max="10"
-              />
-            </td>
-            <td>£ {{ ((item.particular.price * item.qty) / 100).toFixed(2) }}</td>
-            <td><i @click="removeItem(item)" class="far fa-trash-alt"></i></td>
-          </tr>
-          <tr>
-            <td></td>
-            <td><strong>Total</strong></td>
-            <td>£ {{ (totalPrice / 100).toFixed(2) }}</td>
-            <td>+ shipping</td>
-          </tr>
-        </tbody>
-      </table>
+    <h1>Your Items</h1>
+    <section class="container">
+      <form action="#">
+        <table cellspacing="0">
+          <tbody>
+            <tr v-for="(item, index) in basketItems" :key="item.particular._id">
+              <td class="image">
+                <div class="img-container">
+                  <img :src="item.product.images[0].thumbnail" :alt="item.product.name" />
+                </div>
+              </td>
+              <td class="details">
+                <div class="name">
+                  <span
+                    ><router-link :to="{ name: 'ShowItem', params: { id: item.product._id } }">{{
+                      item.product.name
+                    }}</router-link></span
+                  >
+                  <span class="size">
+                    <em>{{ item.particular.size != 'N/A' ? item.particular.size : '' }}</em></span
+                  >
+                </div>
+              </td>
+              <td class="qty">
+                <select
+                  :name="`${index}-qty`"
+                  :id="`${index}-qty`"
+                  v-model="item.qty"
+                  @change="changeQuantity(item, $event)"
+                >
+                  <option :selected="item.qty === 1" value="1">1</option>
+                  <option :selected="item.qty === 2" value="2">2</option>
+                  <option :selected="item.qty === 3" value="3">3</option>
+                  <option :selected="item.qty === 4" value="4">4</option>
+                  <option :selected="item.qty === 5" value="5">5</option>
+                  <option :selected="item.qty === 6" value="6">6</option>
+                  <option :selected="item.qty === 7" value="7">7</option>
+                  <option :selected="item.qty === 8" value="8">8</option>
+                  <option :selected="item.qty === 9" value="9">9</option>
+                  <option :selected="item.qty === 10" value="10">10</option>
+                </select>
+              </td>
+              <td>£ {{ (item.particular.price / 100).toFixed(2) }}</td>
+              <td><i @click="removeItem(item)" class="far fa-trash-alt"></i></td>
+            </tr>
+            <tr class="final-row">
+              <td class="empty"></td>
+              <td class="empty"></td>
+              <td><strong>Total</strong></td>
+              <td class="total-price">£ {{ (totalPrice / 100).toFixed(2) }}</td>
+              <td>+ shipping</td>
+            </tr>
+          </tbody>
+        </table>
+        <div class="checkout">
+          <router-link :to="{ name: 'Checkout' }"
+            ><button>Continue to Checkout</button></router-link
+          >
+        </div>
+      </form>
     </section>
-    <div class="checkout">
-      <router-link :to="{ name: 'Checkout' }"><button>Continue to Checkout</button></router-link>
-    </div>
   </div>
 </template>
 
@@ -80,6 +99,78 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+h1 {
+  margin-top: 5em;
+  margin-left: 10vw;
+}
+.container {
+  width: 60%;
+  margin: 5vh auto;
+  background: white;
+  border-radius: 10px;
+  padding: 10px;
+
+  table {
+    width: 95%;
+    margin: 10px auto;
+    td {
+      text-align: center;
+      &:not(.empty) {
+        border-bottom: 1px solid #ddd;
+      }
+      &.details {
+        .name {
+          display: flex;
+          flex-direction: column;
+          align-items: flex-start;
+          .size {
+            color: $subInfo;
+          }
+          a {
+            // text-decoration: none;
+            cursor: pointer;
+            color: #66669c;
+          }
+        }
+      }
+
+      &.image {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        // border-right: 1px solid grey;
+        .img-container {
+          height: 130px;
+          width: 130px;
+          margin: 10px 0;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          img {
+            max-width: 100%;
+            max-height: 100%;
+          }
+        }
+      }
+
+      &.qty {
+        select {
+          padding: 2px;
+          border-radius: 4px;
+          background: white;
+        }
+      }
+
+      &.total-price {
+        min-width: 80px;
+      }
+    }
+    .final-row {
+      height: 40px;
+    }
+  }
+}
+
 i {
   cursor: pointer;
 }
