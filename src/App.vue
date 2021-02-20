70d51ebfde44eea7789a0e7081a11afb1b8c412c
Karan Basu <karan.basu16@gmail.com>
2021-02-20 17:28:44 +0000

    changed background

--- src/App.vue
+++ src/App.vue
@@ -27,8 +27,11 @@ html {
   overflow-x: hidden;
 
   body {
-    background: url('https://res.cloudinary.com/dqyymjqpg/image/upload/v1612965186/Bridgo/statics/A5AADBDA-339A-46B0-82E3-B696A9B826CB_vkvamo.jpg');
+    // background: url('https://res.cloudinary.com/dqyymjqpg/image/upload/v1612965186/Bridgo/statics/A5AADBDA-339A-46B0-82E3-B696A9B826CB_vkvamo.jpg');
+    // background: url('https://res.cloudinary.com/dqyymjqpg/image/upload/v1613759104/Bridgo/statics/Background1_zqy8ai.jpg');
     background-size: cover;
+    background-position: center;
+    background-attachment: fixed;
   }
 
   #base {
