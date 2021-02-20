0beadf3cc3cfcf02126d53354e10ae0423d681c7
Karan Basu <karan.basu16@gmail.com>
2021-02-20 17:33:23 +0000

    hover image only takes up width of initial image

--- src/components/Portfolio/PortfolioViewAll.vue
+++ src/components/Portfolio/PortfolioViewAll.vue
@@ -1,25 +1,32 @@
 <template>
-  <div class="container">
-    <div class="project-window" v-for="project in projects" :key="project._id">
-      <router-link :to="{ name: 'ShowPortfolio', params: { id: project._id } }">
-        <div class="img-box">
-          <img :src="project.images[0].imgURL" />
-          <div class="img-info">
-            <h3>{{ project.name }}</h3>
-            <!-- <h4>{{ project.materials }}</h4> -->
-            <h4>{{ project.year }}</h4>
+  <div class="view-all">
+    <div class="container">
+      <div class="project-window" v-for="project in projects" :key="project._id">
+        <router-link :to="{ name: 'ShowPortfolio', params: { id: project._id } }">
+          <div class="img-box">
+            <img :src="project.images[0].imgURL" />
+            <div class="img-info">
+              <h3>{{ project.name }}</h3>
+              <!-- <h4>{{ project.materials }}</h4> -->
+              <em
+                ><h4>{{ project.year }}</h4></em
+              >
+            </div>
           </div>
-        </div>
-      </router-link>
+        </router-link>
+      </div>
     </div>
-    <button @click="toggleScrollView">Scroll View</button>
+    <StandardButton class="toggle-view" @click="toggleScrollView">Change View</StandardButton>
   </div>
 </template>
 
 <script>
+import StandardButton from '@/components/Buttons/StandardButton.vue'
+
 export default {
   name: 'PortfolioViewAll',
   props: { projects: Array },
+  components: { StandardButton },
   emits: ['toggle-scroll-view'],
   methods: {
     toggleScrollView() {
@@ -33,23 +40,31 @@ export default {
 .container {
   display: grid;
   grid-template-columns: repeat(3, 1fr);
-  margin: 4vw auto;
+  margin: 10vh auto;
   width: 68vw;
   grid-gap: 20px;
+
   .project-window {
-    width: 288px;
-    height: 246px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    height: 280px;
+    width: 285px;
+    position: relative;
     .img-box {
-      position: relative;
-      cursor: pointer;
-      width: 288px;
-      height: 246px;
+      max-height: 260px;
+      max-width: 260px;
+      height: auto;
+      width: auto;
       display: flex;
       align-items: center;
       justify-content: center;
+      position: relative;
+      cursor: pointer;
 
       img {
-        max-height: 100%;
+        max-height: 260px;
         max-width: 100%;
         display: block;
       }
@@ -57,16 +72,15 @@ export default {
       .img-info {
         width: 100%;
         height: 100%;
-        background-color: rgba(0, 0, 0, 0.596);
+        background-color: rgba(0, 0, 0, 0.7);
         color: white;
         position: absolute;
-        top: 0;
-        opacity: 0;
-        transition: opacity 0.5s ease;
         display: flex;
-        flex-direction: column;
         align-items: center;
         justify-content: center;
+        flex-direction: column;
+        opacity: 0;
+        transition: all 0.5s ease-out;
 
         &:hover {
           opacity: 1;
@@ -74,5 +88,10 @@ export default {
       }
     }
   }
+}
+.toggle-view {
+  position: absolute;
+  bottom: 10vh;
+  right: 6vw;
 }
 </style>
