20a541200ab9419c08c92d6e8e53d0cd6f6e6e34
Karan Basu <karan.basu16@gmail.com>
2021-02-20 17:27:52 +0000

    created pay now component

---
+++ src/components/Buttons/PayNowButton.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <button type="submit" class="btn">
+      <i class="fas fa-lock"></i>Pay <strong>£ {{ amount }}</strong>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  props: {
+    amount: String
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.btn {
+  padding: 8px 20px;
+  border-radius: 30px;
+  border: 2px solid grey;
+  overflow: hidden;
+  width: 200px;
+  height: 80px;
+  background: rgba(0, 0, 0, 0);
+  font-size: 15px;
+  color: grey;
+
+  i {
+    color: grey;
+    padding-right: 10px;
+  }
+}
+</style>
