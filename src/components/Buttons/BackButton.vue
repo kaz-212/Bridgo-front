38f3b09c1fd4cb21d7fd19cb1922f7e7dccaab68
Karan Basu <karan.basu16@gmail.com>
2021-02-25 22:56:32 +0000

    Styled back buttonand added text prop

--- src/components/Buttons/BackButton.vue
+++ src/components/Buttons/BackButton.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     <router-link :to="{ name: whereTo }"
-      ><button><i class="fas fa-sm fa-chevron-left"></i> Back</button></router-link
+      ><i class="left fas fa-arrow-left"></i>
+      <h3>{{ text }}</h3></router-link
     >
   </div>
 </template>
@@ -9,8 +10,29 @@
 <script>
 export default {
   name: 'BackButton',
-  props: { whereTo: String }
+  props: { whereTo: String, text: String }
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+div {
+  transition: all 0.2s;
+  a {
+    text-decoration: none;
+    h3 {
+      display: inline;
+      transition: all 0.5s ease-out;
+      color: #757575;
+    }
+    .left {
+      margin-right: 7px;
+      transition: all 0.5s ease-out;
+      color: #757575;
+    }
+  }
+  &:hover {
+    box-shadow: 0 0.5em 0.5em -0.4em #2f302c;
+    transform: translateY(-0.15em);
+  }
+}
+</style>
