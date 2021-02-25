9283906f6a7bd9b1e985fac51b3ec87008bb78d5
Karan Basu <karan.basu16@gmail.com>
2021-02-25 23:00:32 +0000

    styled details tab and added back button

--- src/views/Exhibitions/ShowExhibition.vue
+++ src/views/Exhibitions/ShowExhibition.vue
@@ -1,36 +1,61 @@
 <template>
   <div v-if="exhibition" id="show-exhibition">
-    <h1>{{ exhibition.name.toUpperCase() }}</h1>
     <div class="container">
-      <section class="main">
-        <article class="exhibition-description">
-          <p>{{ exhibition.description }}</p>
-        </article>
-        <article class="image-container">
-          <div class="image" v-for="image in exhibition.images" :key="image.filename">
-            <img :src="image.thumbnail" alt="exhibition image" />
+      <div class="wrapper">
+        <BackButton class="back" whereTo="OnShowExhibitions" text="ALL EXHIBITIONS" />
+        <section class="main">
+          <h1>{{ exhibition.name.toUpperCase() }}</h1>
+          <article class="exhibition-description">
+            <p>{{ exhibition.description }}</p>
+          </article>
+        </section>
+        <section class="side">
+          <div class="content">
+            <!-- <h3 class="heading">EXHIBITION DETAILS</h3> -->
+            <div class="details-grid">
+              <h4>
+                <strong class="label">Location: </strong>
+              </h4>
+              <h4>
+                <em>{{ exhibition.location }}</em>
+              </h4>
+              <h4>
+                <strong class="label">Date:</strong>
+              </h4>
+              <h4>
+                <em>{{ exhibition.date }}</em>
+              </h4>
+              <h4 v-if="exhibition.links.length">
+                <strong class="label">Links:</strong>
+              </h4>
+              <div class="all-links" v-if="exhibition.links.length">
+                <a
+                  v-for="link in exhibition.links"
+                  :key="link.name"
+                  :href="link.URL"
+                  target="_blank"
+                  >{{ link.name }}
+                </a>
+              </div>
+            </div>
           </div>
-        </article>
-      </section>
-      <section class="side">
-        <h3>When?</h3>
-        <p>{{ exhibition.date }}</p>
-        <h3>Where?</h3>
-        <p>{{ exhibition.location }}</p>
-        <div v-if="exhibition.links.length > 0" class="links">
-          <h3>Links:</h3>
-          <a v-for="link in exhibition.links" :key="link.name" :href="link.URL" target="_blank"
-            >{{ link.name }}
-          </a>
+        </section>
+      </div>
+      <article class="image-container">
+        <div class="image" v-for="image in exhibition.images" :key="image.filename">
+          <img :src="image.thumbnail" alt="exhibition image" />
         </div>
-      </section>
+      </article>
     </div>
   </div>
 </template>
 
 <script>
+import BackButton from '@/components/Buttons/BackButton.vue'
+
 export default {
   name: 'ShowExhibition',
+  components: { BackButton },
   computed: {
     id() {
       return this.$route.params.id
@@ -44,30 +69,113 @@ export default {
 
 <style lang="scss" scoped>
 .container {
-  display: flex;
+  margin-top: $navheight;
+  position: relative;
 
-  .main {
-    margin-top: 3vh;
-    margin-left: 6vw;
+  .wrapper {
+    display: flex;
+    margin: 0 auto;
+    width: 90vw;
 
-    .exhibition-description {
-      // background-color: red;
-      width: 65vw;
-      margin-top: 7vh;
-      p {
-        color: grey;
-        white-space: pre-wrap;
+    .back {
+      position: absolute;
+      top: 30px;
+      right: 30px;
+    }
+    .main {
+      margin-top: 10vh;
+      margin-left: 6vw;
+
+      h1 {
+        color: gray;
+        text-shadow: 0 0 2px #996b6b75;
+        font-size: 32px;
+      }
+      .exhibition-description {
+        // background-color: red;
+        margin: 8vh auto;
+        width: 40vw;
+        min-height: 200px;
+        text-align: justify;
+
+        p {
+          color: #464646;
+          white-space: pre-wrap;
+        }
       }
     }
+    .side {
+      border-radius: 15px;
+      margin: 26vh auto 30px auto;
+      min-height: 200px;
+      width: 380px;
+      height: 200px;
+      background: -webkit-linear-gradient(to bottom, #79d3e157, #c187ca4f);
+      background: linear-gradient(to bottom, #79d3e157, #c187ca4f);
+      animation: boxShade 2s ease forwards;
 
-    .image-container {
-      margin: 10vh 0;
+      padding: 30px 50px;
+      position: relative;
+
       display: flex;
-      // background-color: olive;
-      justify-content: center;
       align-items: center;
-      .image {
-        margin-right: 1vw;
+
+      i {
+        color: white;
+        position: absolute;
+        top: 15px;
+        left: 19px;
+        font-size: 18px;
+        cursor: pointer;
+        text-shadow: 0 0 2px black;
+      }
+      .content {
+        margin: auto 0;
+        .heading {
+          padding: 15px;
+          border-radius: 10px;
+          background-color: #80808027;
+          font-weight: 600;
+          letter-spacing: 3px;
+          color: white;
+          text-shadow: 0 0 2px black;
+          margin-bottom: 20px;
+          text-align: center;
+          align-self: flex-start;
+        }
+        .details-grid {
+          display: grid;
+          grid-template-columns: 90px 1fr;
+          grid-gap: 10px;
+          h4 {
+            margin: 10px auto 0 0;
+            .label {
+              color: white;
+              text-shadow: 0 0 2px black;
+            }
+          }
+          .all-links {
+            a {
+              margin-top: auto;
+              display: block;
+              // margin-left: 30px;
+            }
+          }
+        }
+      }
+    }
+  }
+  .image-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    max-width: 100%;
+    height: auto;
+    .image {
+      margin-right: 1vw;
+      max-height: 150px;
+      img {
+        max-height: 150px;
       }
     }
   }
