f71ff0e8a2bca308bd9f591f281529bf001fd598
Karan Basu <karan.basu16@gmail.com>
2021-03-01 18:57:03 +0000

    changed background image

--- src/App.vue
+++ src/App.vue
@@ -27,8 +27,7 @@ html {
   overflow-x: hidden;
 
   body {
-    background-image: linear-gradient(rgba(249, 249, 249, 0.5), rgba(201, 201, 255, 0.13)),
-      url('https://res.cloudinary.com/dqyymjqpg/image/upload/v1613992704/Bridgo/statics/E8B0536F-0EA4-4204-9875-94BA57DDD4CA_yvz2mc.jpg');
+    background-image: url('https://res.cloudinary.com/bridget-simpson-art/image/upload/w_auto,q_auto/v1614603594/Bridgo/statics/Background.jpg');
     background-size: cover;
     background-position: center;
     background-attachment: fixed;
