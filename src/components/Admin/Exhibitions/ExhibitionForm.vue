bd656e9a44fc90cdb3404249f595ca695cfb6f1e
Karan Basu <karan.basu16@gmail.com>
2021-03-07 15:42:28 +0000

    styled exhibition form

--- src/components/Admin/Exhibitions/ExhibitionForm.vue
+++ src/components/Admin/Exhibitions/ExhibitionForm.vue
@@ -1,38 +1,42 @@
 <template>
-  <div class="form" action="#">
-    <TextInput id="name" label="Name" v-model="exhibition.name" />
-    <TextInput id="date" label="Date" v-model="exhibition.date" />
+  <div action="#">
+    <BackButton whereTo="AllExhibitions" text="All Exhibitions" class="back" />
+    <form action="#" class="form">
+      <TextInput id="name" label="Name" v-model="exhibition.name" />
+      <TextInput id="date" label="Date" v-model="exhibition.date" />
 
-    <TextInput id="location" label="Location" v-model="exhibition.location" />
+      <label class="form-item" for="location">Location</label>
+      <textarea id="location" v-model="exhibition.location" rows="5" required />
+      <label class="form-item" for="description">Description</label>
+      <textarea id="description" v-model="exhibition.description" rows="5" required />
 
-    <label class="form-item" for="description">Description</label>
-    <textarea id="description" v-model="exhibition.description" rows="5" required />
-
-    <label class="form-item" for="links">Links</label>
-    <div id="links" v-for="(link, index) in exhibition.links" :key="index">
-      <em>Link {{ index + 1 }} <i @click="deleteLink(index)" class="far fa-trash-alt"></i></em>
-      <input type="text" v-model="link.name" placeholder="name" />
-      <input type="text" v-model="link.URL" placeholder="URL" />
-    </div>
-    <button v-if="exhibition.links.length === 0" @click.prevent="addLink">Add Link</button>
-    <button v-if="exhibition.links.length > 0" @click.prevent="addLink">Add Another Link</button>
-    <ImageUpload v-model="imgs" />
-    <label for="Show">Show this exhibition?</label>
-    <input class="check-box" v-model="exhibition.onShow" id="show" type="checkbox" checked />
-    <label for="Show">Is this an upcoming Exhibition?</label>
-    <input class="check-box" v-model="exhibition.isUpcoming" id="show" type="checkbox" checked />
-    <button @click.prevent="submitExhibition">Submit</button>
-    <button @click.prevent="cancel">Back</button>
+      <label class="form-item" for="links">Links</label>
+      <div id="links" v-for="(link, index) in exhibition.links" :key="index">
+        <em>Link {{ index + 1 }} <i @click="deleteLink(index)" class="far fa-trash-alt"></i></em>
+        <input type="text" v-model="link.name" placeholder="name" />
+        <input type="text" v-model="link.URL" placeholder="URL" />
+      </div>
+      <button v-if="exhibition.links.length === 0" @click.prevent="addLink">Add Link</button>
+      <button v-if="exhibition.links.length > 0" @click.prevent="addLink">Add Another Link</button>
+      <ImageUpload v-model="imgs" />
+      <label for="Show">Show this exhibition?</label>
+      <input class="check-box" v-model="exhibition.onShow" id="show" type="checkbox" checked />
+      <label for="Show">Is this an upcoming Exhibition?</label>
+      <input class="check-box" v-model="exhibition.isUpcoming" id="show" type="checkbox" checked />
+      <button @click.prevent="submitExhibition">Submit</button>
+      <button @click.prevent="cancel">Back</button>
+    </form>
   </div>
 </template>
 
 <script>
 import TextInput from '@/components/form/TextInput.vue'
 import ImageUpload from '@/components/form/ImageUpload.vue'
+import BackButton from '@/components/Buttons/BackButton.vue'
 
 export default {
   name: 'ExhibitionForm',
-  components: { TextInput, ImageUpload },
+  components: { TextInput, ImageUpload, BackButton },
   props: {},
   data() {
     return {
@@ -92,13 +96,26 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.back {
+  position: absolute;
+  width: 200px;
+  right: 0;
+}
+
 .form {
-  #image {
-    display: none;
-  }
+  width: 90%;
+  margin: 3% auto;
 
   i {
     cursor: pointer;
+  }
+
+  label {
+    margin-top: 10px;
+  }
+
+  button {
+    max-width: 200px;
   }
 }
 </style>
