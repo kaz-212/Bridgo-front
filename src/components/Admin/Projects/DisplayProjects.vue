f9be05593dfbd78f70365111813efe9de5a5d87c
Karan Basu <karan.basu16@gmail.com>
2021-03-07 15:10:06 +0000

    projects displayed side by side

--- src/components/Admin/Projects/DisplayProjects.vue
+++ src/components/Admin/Projects/DisplayProjects.vue
@@ -1,14 +1,12 @@
 <template>
-  <div>
-    <Draggable v-model="projects" item-key="name" class="list-group">
+  <div class="container">
+    <Draggable v-model="projects" item-key="name" class="list-group grid">
       <template #item="{ element }">
-        <div class="list-group-item grid">
-          <div class="project">
-            <router-link :to="{ name: 'EditProject', params: { id: element._id } }"
-              ><img :src="element.images[0].thumbnail"
-            /></router-link>
-            <h4>{{ element.name }}</h4>
-          </div>
+        <div class="list-group-item grid-container">
+          <router-link :to="{ name: 'EditProject', params: { id: element._id } }"
+            ><img :src="element.images[0].thumbnail"
+          /></router-link>
+          <h4>{{ element.name }}</h4>
         </div>
       </template>
     </Draggable>
@@ -47,4 +45,20 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.container {
+  width: 80%;
+  margin: 7% auto 0 auto;
+  .grid {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 20px;
+    .grid-container {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+    }
+  }
+}
+</style>
